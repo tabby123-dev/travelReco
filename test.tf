b4210635997b95f6d4c656d8cc24cf41f8fc2f27
tabithawanjikundungu@gmail.com <tabithawanjikundungu@gmail.com>
2025-02-18 11:53:10 +0300

    Check for commint conflicts

--- test.tf
+++ test.tf
@@ -11,9 +11,7 @@ resource "aws_instance" "example" {
   tags = {
     Name = "MyExampleInstance"
   }
+#chnaging this file to test something
+
 }
 
-# Output the public IP of the instance
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
-}
